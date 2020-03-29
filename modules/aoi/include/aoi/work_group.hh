a4afa5a0b301aac97a20b9187ad03f7b3205097b
Code Monad <code@lab-11.org>
2020-03-30 02:47:16 +0800

    fixed worked group won't dispose chained tasks.

--- modules/aoi/include/aoi/work_group.hh
+++ modules/aoi/include/aoi/work_group.hh
@@ -9,18 +9,23 @@
 
 namespace aoi {
   class basic_task;
+
+  // chained tasks, like a linked list
   class work_group {
   public:
     work_group(std::shared_ptr<boost::asio::io_context> executor);
     void add_task(std::shared_ptr<basic_task> task);
 
     std::shared_ptr<basic_task> begin() noexcept;
+    std::shared_ptr<basic_task> last() noexcept;
+    std::shared_ptr<basic_task> end() noexcept { return std::shared_ptr<basic_task>(nullptr); }
 
     void execute();
 
   private:
     std::shared_ptr<boost::asio::io_context> _executor;
     std::shared_ptr<basic_task> _begin = nullptr;
+    std::shared_ptr<basic_task> _last = nullptr;
     std::map<boost::uuids::uuid, std::shared_ptr<basic_task>> _tasks;
   };
 }
--- modules/aoi/src/work_group.cc
+++ modules/aoi/src/work_group.cc
@@ -4,8 +4,12 @@
 aoi::work_group::work_group(std::shared_ptr<boost::asio::io_context> executor) : _executor{executor}, _tasks{} {}
 
 void aoi::work_group::add_task(std::shared_ptr<basic_task> task) {
-  if(_tasks.empty())
+  if(_tasks.empty()) {
     _begin = task;
+  } else {
+    _last->set_next(task);
+  }
+  _last = task;
   _tasks[task->uuid()] = task;
   task->set_executor(_executor);
 }
@@ -15,7 +19,14 @@ std::shared_ptr<aoi::basic_task> aoi::work_group::begin() noexcept {
     return _begin;
 }
 
+std::shared_ptr<aoi::basic_task> aoi::work_group::last() noexcept {
+    return _last;
+}
+
+
 void aoi::work_group::execute() {
-  if(this->begin())
-    this->begin()->process();
+
+  for(auto task = this->begin(); task != this->end(); task = task->next()){
+    task->process();
+  }
 }
